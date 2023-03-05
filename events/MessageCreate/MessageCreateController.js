391558b3c1adc7bc74e135f7635bf12d46e02e8e
Zhenya <Canimore@yandex.com>
2023-03-05 21:41:42 +0300

    update

--- events/MessageCreate/MessageCreateController.js
+++ events/MessageCreate/MessageCreateController.js
@@ -7,17 +7,22 @@ class MessageCreateController {
         this.comman = new CommandManager(models);
     }
     func(message) {
-        //проверка на бота
-        if (
-            message.member.user.bot ||
-            message.member.id === process.env.APPID
-        ) {
-            return;
+        try {
+            //проверка на бота
+            if (
+                !message.member ||
+                message.member.user.bot ||
+                message.member.id === process.env.APPID
+            ) {
+                return;
+            }
+            if (!message.content.startsWith(".")) {
+                return;
+            }
+            this.comman.run(message);
+        } catch (err) {
+            console.log(err);
         }
-        if (!message.content.startsWith(".")) {
-            return;
-        }
-        this.comman.run(message);
     }
     getName() {
         return "messageCreateController";
