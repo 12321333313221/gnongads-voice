746f2630eb1b85b7b153c6e1f463b208bc2a2a43
Zhenya <Canimore@yandex.com>
2023-03-05 20:35:15 +0300

    additional for #12

--- events/MessageCreate/commands/kick/kick.js
+++ events/MessageCreate/commands/kick/kick.js
@@ -3,11 +3,14 @@ module.exports = {
     description: `Функционал для работы с кикам`,
     alias: [".kick", ".kicks", ".kic"],
     subcommands: require("./subcommands"),
+    roles: ["1078705130972647565", "1081615301738766392", "954393422716879019"],
     verify: function (str) {
         return this.alias.find((alias) => alias === str);
     },
     hasAccess: function (roles) {
-        return true;
+        return roles.find((role) =>
+            this.roles.find((trole) => role.id === trole)
+        );
     },
     getSubcommand: function (commands) {
         if (commands.length === 2) {
--- events/MessageCreate/commands/voice/voice.js
+++ events/MessageCreate/commands/voice/voice.js
@@ -3,12 +3,14 @@ module.exports = {
     description: `Функционал для установления лимита установ`,
     alias: [".voice", ".vc"],
     subcommands: require("./subcommands"),
-    roles: [],
+    roles: ["1078705130972647565", "1081615301738766392", "954393422716879019"],
     verify: function (str) {
         return this.alias.find((alias) => alias === str);
     },
     hasAccess: function (roles) {
-        return true;
+        return roles.find((role) =>
+            this.roles.find((trole) => role.id === trole)
+        );
     },
     getSubcommand: function (str) {
         return this.subcommands.find((act) => act.name === str[1]);
